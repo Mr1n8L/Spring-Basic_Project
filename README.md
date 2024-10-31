1acef2ac4921b937ff2b2dd5b2447ab9005e496f
Mr1n8L <mrinaljani1822@gmail.com>
2024-10-31 21:26:13 +0530

    backend-frontend changes...

--- README.md
+++
@@ -1,2 +0,0 @@
-# Spring-Basic_Project
-Building a basic e-commerce website
--- src/main/java/com/basic/Spring_Basic_project/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.basic.Spring_Basic_project.repository;
-
-import com.basic.Spring_Basic_project.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-}
